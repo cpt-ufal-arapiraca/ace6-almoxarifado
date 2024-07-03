d27638d6d9f75a7feb88a68e2b893c4db718396a
Uanderson <uanderson_pb@hotmail.com.br>
2024-07-03 18:06:08 -0300

    att

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/index.html', status=201)
 
-def contato(contato):
+def contato(request):
     return HttpResponse('contato')
 
-def sobre(sobre):
+def sobre(request):
     return HttpResponse('sobre')
