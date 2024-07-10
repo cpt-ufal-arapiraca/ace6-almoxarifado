9a97445c4613312f31c9578f63b0b0aaf1ff93fa
Uanderson <uanderson_pb@hotmail.com.br>
2024-07-10 03:02:54 -0300

    teste

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_person, make_sector
+
 
 def login(request):
     return render(request, 'recipes/pages/login.html')
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html',context={
+        'person':make_person(),
+        'boxOptions':[make_sector() for _ in range(6)],
+
+    })
 
 def config(request):
     return render(request, 'recipes/pages/config.html')
